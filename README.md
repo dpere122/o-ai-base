61037b29b5fff309b7e2834c36f5d31a2df79c1b
dpere122 <dpere381@fiu.edu>
2023-05-23 17:58:52 -0400

    Chatgpt now returns a response to from rest endpoint

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # o-ai-base
 
-## References and libraries Listed:
+### References and libraries Listed:
 
 - ExpressJS Library: https://github.com/expressjs/express
 - OpenAi Nodejs Library: https://github.com/openai/openai-node
--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,41 @@
 import express from 'express'
 import https from 'https'
+import {Configuration, OpenAIApi} from 'openai'
+import dotenv from 'dotenv'
 
 //The goal of this middle man service is to ensure the data being sent is logged on our end
 //Secondly we need to ensure that the openai bot is staying on topic
 //Thirdly we need this to ensure that our keys are not saved on front-end systems
 
 const app = express()
+dotenv.config()
 app.use(express.json());
 
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+async function iniChat(message:string){
+    await JSON.stringify(openai.createChatCompletion({
+        model: 'gpt-3.5-turbo',
+        messages: [{role:'user',content:message}]
+    }).then((response)=>{
+        console.log(response.data.choices[0].message);
+    }));
+};
+
+
 app.get('/',(req,rest)=>{
-    return rest.send("Hello World");
+    return rest.send("Welcome to the simple OpenAI Chat model!");
 });
 
-app.post('/api/data',(req,res)=>{
-    console.log(req.body);
-
+app.post('/api/data',async (req,res)=>{
+    const chatInput = JSON.stringify(req.body.message);
+    console.log(chatInput);
+    iniChat(chatInput)
+    // const parsedRes = JSON.parse(await iniChat(chatInput));
+    // console.log(parsedRes.data);
     return res.sendStatus(200);
 });
 
